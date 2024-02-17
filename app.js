75a3f813d224782dd7a1a0324c5bc16e436e2396
Oshayer Siddique <oshayer.siddique2001@gmail.com>
2024-02-18 04:24:41 +0600

    18

--- app.js
+++ app.js
@@ -107,6 +107,89 @@ app.post("/analyzeAudio", upload.array("files"), async (req, res) => {
 });
 
 
+async function voicechattxt(reply) {
+  const prompt = reply;
+
+  try {
+      const response = await openai.chat.completions.create({
+          model: "gpt-4-vision-preview",
+          max_tokens: 100,
+          messages: [
+              {
+                  role: "user",
+                  content: [
+                      { type: "text", text: prompt },
+                  ],
+              },
+          ],
+      });
+
+      return response.choices[0];
+  } catch (error) {
+      console.error('Error generating text:', error);
+      throw new Error("Error generating text");
+  }
+}
+
+
+async function chattospeech(userinput) {
+  try {
+      const mp3 = await openai.audio.speech.create({
+          model: "tts-1",
+          voice: "fable",
+          input: userinput,
+      });
+
+      return Buffer.from(await mp3.arrayBuffer());
+  } catch (error) {
+      console.error('Error generating MP3 file:', error);
+      throw new Error("Error generating MP3 file");
+  }
+}
+
+
+
+
+app.post("/voicechat", upload.array("files"), async (req, res) => {
+  // Sets multer to intercept files named "files" on uploaded form data
+
+  let audio = req.files[0];
+  if (!(audio.mimetype.includes('audio'))) {
+    fs.unlink(audio.path, (err) => {
+      if (err) {
+        console.error(err)
+      }
+    })
+    res.json({ message: "File(s) must be an audio" });
+  }
+  else {
+    const transcription = await openai.audio.transcriptions.create({
+      file: fs.createReadStream(audio.path),
+      model: "whisper-1",
+      response_format: "verbose_json",
+    });
+
+    let reply = transcription.text;
+
+
+    fs.unlink(audio.path, (err) => {
+      if (err) {
+        console.error(err)
+      }
+    })
+    console.log(reply);
+    let gptreply = await voicechattxt(reply);
+    let gptvoice = await chattospeech(gptreply.message.content);
+    res.setHeader('Content-Type', 'audio/mpeg');
+
+    // Send the buffer as the response
+    res.end(gptvoice);
+    
+  }
+
+});
+
+
 
 
 const generateTextRouter = require('./routers/Generate_text_router');
